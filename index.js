5a6f219bc63cd7d6d189977a33f977557f981f34
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-04-02 10:39:28 -0700

    es linted

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d"); // object that we can call methods on
-let x = canvas.width/2;
-let y = canvas.height-30;
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d'); // object that we can call methods on
+let x = canvas.width / 2;
+let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
@@ -9,106 +9,102 @@ let dy = -2;
 const ballRadius = 10;
 
 // paddle variables
-let paddleHeight = 10;
-let paddleWidth = 75;
-let paddleX = (canvas.width-paddleWidth) / 2;
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
 
 
 function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
 }
 
 function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
 }
 
 function draw() {
-    // drawing code
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawPaddle();
-    x += dx;
-    y += dy;
+  // drawing code
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  drawPaddle();
+  x += dx;
+  y += dy;
 
-    if(x + dx > canvas.width-ballRadius || x + dx < 0) {
-        dx = -dx;
-        ctx.fillStyle = "#green";
-        ctx.fill();
+  if (x + dx > canvas.width - ballRadius || x + dx < 0) {
+    dx = -dx;
+    ctx.fillStyle = '#green';
+    ctx.fill();
+  }
 
-    }
-    if(y + dy > canvas.height-ballRadius || y + dy < 0) {
-        dy = -dy;
-        ctx.fillStyle = "#yellow";
-        ctx.fill();
+  if (y + dy > canvas.height-ballRadius || y + dy < 0) {
+    dy = -dy;
+    ctx.fillStyle = '#yellow';
+    ctx.fill();
+  }
 
+  if (rightPressed) {
+    paddleX += 7;
+    if (paddleX + paddleWidth > canvas.width) {
+      paddleX = canvas.width - paddleWidth;
     }
-
-    if(rightPressed) {
-        paddleX += 7;
-        if (paddleX + paddleWidth > canvas.width){
-            paddleX = canvas.width - paddleWidth;
-        }
+  } else if (leftPressed) {
+    paddleX -= 7;
+    if (paddleX < 0) {
+      paddleX = 0;
     }
-    else if(leftPressed) {
-        paddleX -= 7;
-        if (paddleX < 0){
-            paddleX = 0;
-        }
-    }
+  }
 }
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-
 function keyDownHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = true;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = true;
-    }
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = true;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = true;
+  }
 }
 
 function keyUpHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = false;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = false;
-    }
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = false;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = false;
+  }
 }
-setInterval(draw, 10);
 
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+setInterval(draw, 10);
 
 
 // previous shape practice
 {/* // red rectangle
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
+ctx.fillStyle = '#FF0000';
 ctx.fill();
 ctx.closePath();
 
 // green circle
 ctx.beginPath();
 ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
+ctx.fillStyle = 'green';
 ctx.fill();
 ctx.closePath();
 
 // blue-stroked empty rectangle 
 ctx.beginPath();
 ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)';
 ctx.stroke();
 ctx.closePath(); */}
